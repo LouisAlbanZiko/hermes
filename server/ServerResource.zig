95e6110cddb0711fe2006684b99242fed0c22491
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2025-05-14 18:09:38 +0200

    Added sentinel character to resources

--- server/ServerResource.zig
+++ server/ServerResource.zig
@@ -1,9 +1,9 @@
 const ServerResource = @This();
 pub const Type = enum { directory, handler, file };
 
-path: []const u8,
+path: [:0]const u8,
 value: union(Type) {
     directory: []const ServerResource,
     handler: type,
-    file: []const u8,
+    file: [:0]const u8,
 },
